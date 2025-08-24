5abe5ced4c582028ea7bf40061edd60a31da9ac6
Rohan-islam-dev <mdrohanislam253@gmail.com>
2025-08-24 21:42:39 +0600

    added btn color use js

--- js/event.js
+++ js/event.js
@@ -1,1 +1,20 @@
-console.log('hello')
+//option 2 event handle event
+function makeRed() {
+  document.body.style.background = "red";
+}
+
+function makeYello() {
+  document.body.style.backgroundColor = "yellow";
+}
+// option 3: get Element by Id and then set onlick
+const btnMakeBlue = document.getElementById("btn-make-blue");
+btnMakeBlue.onclick = function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+};
+
+// option 3
+const btnMakePurple = document.getElementById("btn-make-purple");
+btnMakePurple.onclick = MakePurple;
+function MakePurple() {
+  document.body.style.backgroundColor = "purple";
+}
